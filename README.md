0d3e6fcc092af9bd9147db5c6c0860d6ff42595f
Cas Pascal <casboi86@gmail.com>
2026-07-04 11:23:31 +0700

    BTA

---
+++ README.md
@@ -0,0 +1,7 @@
+# FiveDurationSwing
+
+If you have a very specific nitpick about the handswing duration being slightly too slow, this mod changes the duration from 6 to 5 to make it faster.
+
+# Better Than Adventure
+
+This version was made specifically for Minecraft BTA. Please don't try to install this anywhere else.
---
+++ src/main/java/dev/sillibeans/slowmine/mixin/BreakSpeedMixin.java
@@ -0,0 +1,33 @@
+package dev.sillibeans.slowmine.mixin;
+
+import net.minecraft.core.entity.player.Player;
+import net.minecraft.core.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(Player.class)
+public class BreakSpeedMixin {
+	@Inject(method = "getCurrentPlayerStrVsBlock", at = @At("RETURN"), cancellable = true)
+	public void changeBlockBreadSpeed(CallbackInfoReturnable<Float> cir) {
+		Player player = (Player) (Object) this;
+		World world = player.world;
+		float multiplier = 1.0f;
+
+		switch (world.getDifficulty()) {
+			case PEACEFUL:
+			case EASY:
+				multiplier = 1.3f;
+				break;
+
+			case HARD:
+				multiplier = 0.6f;
+				break;
+
+			default: {}
+		}
+
+		cir.setReturnValue(cir.getReturnValue() * multiplier);
+	}
+}
